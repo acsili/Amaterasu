d51daa00ab7b8fdf700fac2c507137f657d703f6
IlyaOvchinnikov-0 <ilyaovchinnikov4@gmail.com>
2023-09-07 12:37:56 +0400

    first commit

---
+++ WPF/Data/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using Amaterasu.Data.DatabaseEntities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Amaterasu.Data;
+
+
+public class ApplicationContext : DbContext
+{
+    readonly string ConnectionString = "Host=localhost;Port=5432;Database=AmaterasuDb;Username=postgres;Password=54321";
+    public DbSet<Word> Words { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(ConnectionString);
+    }
+}
+
+    
+
---
+++ WPF/Data/DatabaseEntities/Word.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amaterasu.Data.DatabaseEntities
+{
+    public class Word
+    {
+        public int Id { get; set; }      
+        public string? WordInJapanese { get; set; }
+        public string? Translation { get; set; }
+        public int Level { get; set; }
+
+    }
+}
