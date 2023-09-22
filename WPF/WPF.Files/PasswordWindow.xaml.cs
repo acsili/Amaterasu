b319f8124b52d8bd6ea7f9de098a2dc3bc6b3cdd
IlyaOvchinnikov-0 <ilyaovchinnikov4@gmail.com>
2023-09-22 20:47:19 +0400

    Add Password Window

---
+++ WPF/WPF.Files/PasswordWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Amaterasu.Data.DatabaseEntities;
+using Amaterasu.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Amaterasu.WPF.Files
+{
+    /// <summary>
+    /// Логика взаимодействия для PasswordWindow.xaml
+    /// </summary>
+    public partial class PasswordWindow : Window
+    {
+        public PasswordWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter && TextBoxPassword.Text != "")
+            {
+                using var db = new ApplicationContext();
+
+
+
+                MessageBox.Show("Слово успешно сохранено");
+            }
+        }
+    }
+}
