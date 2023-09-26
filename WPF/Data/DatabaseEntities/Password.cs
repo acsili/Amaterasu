a879c242f080b4f967f4b779d2a252cec31cf6c2
IlyaOvchinnikov-0 <ilyaovchinnikov4@gmail.com>
2023-09-26 16:32:31 +0400

    Entity of Password

---
+++ WPF/Data/DatabaseEntities/Password.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Amaterasu.Data.DatabaseEntities
+{
+    [Table("Passwords")]
+    public class Password
+    {
+        public int Id { get; set; }
+        [MaxLength(15)]
+        public string? Value { get; set; }
+    }
+}
