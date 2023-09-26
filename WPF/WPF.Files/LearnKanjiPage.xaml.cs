0da2c212156bdf811d1bc08b81a836f046634d0b
IlyaOvchinnikov-0 <ilyaovchinnikov4@gmail.com>
2023-09-26 21:10:07 +0400

    Learn kanji page

---
+++ WPF/WPF.Files/LearnKanjiPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Amaterasu.Data;
+using System;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace Amaterasu.WPF.Files
+{
+    public partial class LearnKanjiPage : Page
+    {
+        public LearnKanjiPage()
+        {
+            InitializeComponent();
+
+            using var db = new ApplicationContext();
+            var kanji = db.Kanji.Select(x => x.Value).First();
+            LabelKanji.Content = kanji;
+        }
+    }
+}
