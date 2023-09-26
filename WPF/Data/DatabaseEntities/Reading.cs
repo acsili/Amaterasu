68038bb0b2dd58aac8161d095c241ac20d2bc647
IlyaOvchinnikov-0 <ilyaovchinnikov4@gmail.com>
2023-09-26 16:32:59 +0400

    Entity of Reading

---
+++ WPF/Data/DatabaseEntities/Reading.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amaterasu.Data.DatabaseEntities
+{
+    public class Reading
+    {
+        public int Id { get; set; }
+        [MinLength(1)]
+        public string? On { get; set; }
+        [MinLength(1)]
+        public string? Kun { get; set; }
+        public int KanjiId { get; set; }
+        public Kanji? Kanji { get; set; }
+    }
+}
