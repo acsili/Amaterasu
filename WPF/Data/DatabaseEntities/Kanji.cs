603c83a99dee3f4fdbed6f47c28406bf7ee48ea7
IlyaOvchinnikov-0 <ilyaovchinnikov4@gmail.com>
2023-09-26 16:31:52 +0400

    Entity of Kanji

---
+++ WPF/Data/DatabaseEntities/Kanji.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amaterasu.Data.DatabaseEntities
+{
+    public class Kanji
+    {
+        public int Id { get; set; }
+        [MinLength(1)]
+        public string? Value { get; set; }
+        [MinLength(1)]
+        public string? Translation { get; set; }
+        public Reading? Reading { get; set; }
+    }
+}
