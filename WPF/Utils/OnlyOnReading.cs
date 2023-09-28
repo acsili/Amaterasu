b54df26942efb20dc87536fcff8a1883bd36a050
IlyaOvchinnikov-0 <ilyaovchinnikov4@gmail.com>
2023-09-28 14:02:31 +0400

    select only on readings

---
+++ WPF/Utils/OnlyOnReading.cs
@@ -0,0 +1,26 @@
+﻿using Amaterasu.Data;
+using Amaterasu.Data.DatabaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amaterasu.Utils;
+
+public class OnlyOnReading : ITeachable
+{
+    public IEnumerable<Kanji> TeachKanji()
+    {
+        using var db = new ApplicationContext();
+
+        var kanji = db.Kanji.Select(x => x).ToList();
+        //TestCreator.Shuffle();
+
+        for (int i = 0; i < kanji.Count; i++)
+        {
+            yield return kanji[i];
+        }
+    }
+}
+
