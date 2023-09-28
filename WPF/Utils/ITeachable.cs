5f8d33f4706df1baf9e5c5bd0a45e97e4797973f
IlyaOvchinnikov-0 <ilyaovchinnikov4@gmail.com>
2023-09-28 14:00:11 +0400

    interface ITeachable

---
+++ WPF/Utils/ITeachable.cs
@@ -0,0 +1,14 @@
+﻿using Amaterasu.Data.DatabaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amaterasu.Utils;
+
+public interface ITeachable
+{
+    public IEnumerable<Kanji> TeachKanji();
+}
+
